a944b8ac16e22dbb4433c5eb835fc86a44dd4c3d
Noah Lett <noahblett96@gmail.com>
2023-09-09 21:22:27 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler')
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions')
+const PORT = process.env.PORT || 3500;
+const app = express();
+
+app.use(logger);
+
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//Static File
+app.use('/', express.static(path.join(__dirname, '/public')));
+
+//Routes
+app.use('/', require('./routes/root'));
+app.use('/register' , require('./routes/api/register'));
+app.use('/employees', require('./routes/api/employees'));
+
+app.all('*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
