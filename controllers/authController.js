c01666d60f9621dc7636fba335c67869e59c7a7a
Noah Lett <noahblett96@gmail.com>
2023-09-21 17:25:04 -0700

    Changed expire time for access token and fixed bug in delete employee endpoint

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,7 +18,7 @@ const handleLogin = async (req, res) => {
                 }
             },
             process.env.ACCESS_TOKEN_SECRET,
-            { expiresIn: '30s' }
+            { expiresIn: '3600s' }
         );
         const refreshToken = jwt.sign(
             { "username": foundUser.username },
