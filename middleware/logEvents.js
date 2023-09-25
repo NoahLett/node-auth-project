a4600b07b8dadbcad560656d72a9660872d26e1c
Noah Lett <noahblett96@gmail.com>
2023-09-25 10:52:39 -0700

    Reverted

--- middleware/logEvents.js
+++ middleware/logEvents.js
@@ -7,7 +7,6 @@ const path = require('path');
 const logEvents = async (message, logName) => {
     const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
     const logItem = `${dateTime}\t${uuid()}\t${message}`;
-    console.log(logItem);
     try {
         if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
             await fsPromises.mkdir(path.join(__dirname, '..', 'logs'));
@@ -20,7 +19,6 @@ const logEvents = async (message, logName) => {
 
 const logger = (req, res, next) => {
     logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt');
-    console.log(`${req.method} ${req.path}`);
     next();
 }
 
