819a52bdacf89d0b4935ba68b6791f20887b226a
Noah Lett <noahblett96@gmail.com>
2023-10-02 19:31:45 -0700

    Fixed everything, hopefully

--- controllers/authController.js
+++ controllers/authController.js
@@ -29,7 +29,7 @@ const handleLogin = async (req, res) => {
         const result = await foundUser.save();
         console.log(result);
         res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 }); // secure: true
-        res.json({ accessToken });
+        res.json({ roles, accessToken });
     } else {
         res.sendStatus(401);
     }
