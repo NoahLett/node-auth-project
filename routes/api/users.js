39a525f5508efb891786089d2cd1705ef1b28662
Noah Lett <noahblett96@gmail.com>
2023-11-01 16:04:51 -0700

    exported userscontroller functions and created users api file

---
+++ routes/api/users.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+const usersController = require('../../controllers/usersController');
+const ROLES_LIST = require('../../config/roles_list');
+const verifyRoles = require('../../middleware/verifyRoles');
