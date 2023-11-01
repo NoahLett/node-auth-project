6d9ab9089ce8abcaddeab9799bfe557b3da6e451
Noah Lett <noahblett96@gmail.com>
2023-11-01 16:28:54 -0700

    added users endpoint to main server file

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,7 +5,7 @@ const ROLES_LIST = require('../../config/roles_list');
 const verifyRoles = require('../../middleware/verifyRoles');
 
 router.route('/')
-    .get(usersController.getAllUsers)
+    .get(verifyRoles(ROLES_LIST.Admin), usersController.getAllUsers)
     .delete(verifyRoles(ROLES_LIST.Admin), usersController.deleteUser);
 
 module.exports = router;
